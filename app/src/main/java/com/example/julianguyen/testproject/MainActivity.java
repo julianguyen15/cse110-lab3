4d5ef79d239b80e3845f6a61a2f5eb2ee96f882f
alvinli222 <33334467+alvinli222@users.noreply.github.com>
2019-01-25 12:18:40 -0800

    Add files via upload

--- app/src/main/java/com/example/julianguyen/testproject/MainActivity.java
+++ app/src/main/java/com/example/julianguyen/testproject/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.julianguyen.testproject;
-
+//alvin's comment
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
